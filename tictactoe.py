0fa8e6f4e8a8736ab198cdc8cd1d2e871feacf1a
Jorge Fuentes <33938341+jorge-fuentes-94@users.noreply.github.com>
2023-07-08 11:02:28 +0200

    Refactor

--- tictactoe.py
+++ tictactoe.py
@@ -5,24 +5,25 @@ def visual(game):
        {game[7]}       {game[8]}       {game[9]}
     """)
 
-def check_win(game):
-    player_one_victory = "XXX"
-    player_two_victory = "OOO"
-
-    if "".join(game[1:3]) == player_one_victory or "".join(game[4:6]) == player_one_victory or "".join(game[7:9]) == player_one_victory:
-        return True
-    elif "".join(game[1]+game[4]+game[7]) == player_one_victory or "".join(game[2]+game[5]+game[8]) == player_one_victory or "".join(game[3]+game[6]+game[9]) == player_one_victory:
-        return True
-    elif "".join(game[1]+game[5]+game[9]) == player_one_victory or "".join(game[3]+game[5]+game[7]) == player_one_victory:
-        return True
-    elif "".join(game[1:3]) == player_one_victory or "".join(game[4:6]) == player_one_victory or "".join(game[7:9]) == player_one_victory:
-        return True
-    elif "".join(game[1]+game[4]+game[7]) == player_two_victory or "".join(game[2]+game[5]+game[8]) == player_two_victory or "".join(game[3]+game[6]+game[9]) == player_two_victory:
-        return True
-    elif "".join(game[1]+game[5]+game[9]) == player_two_victory or "".join(game[3]+game[5]+game[7]) == player_two_victory:
-        return True
+def check_win(game,player):
+    player_victory = ""
+    winner = False
+    fields_to_check = ["".join(game[1]+game[2]+game[3]),"".join(game[4]+game[5]+game[6]),"".join(game[7]+game[8]+game[9]),"".join(game[1]+game[4]+game[7]),"".join(game[2]+game[5]+game[8]),"".join(game[3]+game[6]+game[9]),"".join(game[1]+game[5]+game[9]),"".join(game[3]+game[5]+game[7])]
+    if player == 1:
+        player_victory = "XXX"
     else:
-        return False
+        player_victory = "OOO"
+
+    for field in fields_to_check:
+        winner = check_win_conditions(player_victory,field)
+        if winner == True:
+            return winner
+
+def check_win_conditions(player_victory,field_to_check):
+    if player_victory == field_to_check:
+        return True
+    return False
+
 
 def isNumber(input):
     if input <=0 or input >9 or type(input) != int:
@@ -63,19 +64,19 @@ winner = False
 players = [1,2]
 
 intro()
-for player in players:
-    if round == 5:
-        print("¡Empate técnico! ¡Nadie gana!")
-        exit()
-    game = play(game,player)
-    visual(game)
-    if round >= 3:
-        winner = check_win(game)
-        if winner == True:
-            print (f"¡El jugador {player} gana!¡Bien jugado!")
-            exit()    
-print("¡Nadie gana! ¡Siguiente ronda!")
-round += 1
+
+while round <5:
+    for player in players:
+        game = play(game,player)
+        visual(game)
+        if round >= 3:
+            winner = check_win(game,player)
+            if winner == True:
+                print (f"¡El jugador {player} gana!¡Bien jugado!")
+                exit()    
+    print("¡Nadie gana! ¡Siguiente ronda!")
+    round += 1
+print("¡Empate técnico! ¡Nadie gana!")
 
     
     
